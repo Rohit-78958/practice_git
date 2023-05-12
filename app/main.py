4a4c9cfcce90a3841a27b62a528bf731e8d1b990
Rohit <kumarohit1202@gmail.com>
2023-05-12 14:37:10 +0530

    handling concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
+
+def handle_connection(client_connection):
+    while True:
+        try:
+            client_connection.recv(1024)
+            client_connection.send(b"+PONG\r\n")
+        except ConnectionError:
+            print("Connection lost")
+            break
 
 
 def main():
@@ -9,10 +19,9 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_conn, _=server_socket.accept() # wait for client
     while True:    
-        client_conn.recv(1024)
-        client_conn.send(b"+PONG\r\n")
+        client_connection, _ = server_socket.accept()
+        threading.Thread(target=handle_connection, args=(client_connection,)).start()
 
 if __name__ == "__main__":
     main()
